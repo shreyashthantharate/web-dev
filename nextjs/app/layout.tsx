fb07ef7605ca8ef548e2c04c150ff61b7a32cf98
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-05-24 18:09:19 +0530

    Add static header and footer heading in layout.tsx

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <h1>Header</h1>
+        {children}
+        <h1>Footer</h1>
+      </body>
     </html>
   );
 }
