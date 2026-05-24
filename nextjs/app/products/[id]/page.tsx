2a9082c46bd617fd0a0e043fb21a3582a84b190b
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-05-24 18:23:29 +0530

    Created first dynamic route with product id page

---
+++ nextjs/app/products/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductIdPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  return <div>ProductIdPage with product id: {id}</div>;
+};
+
+export default ProductIdPage;
