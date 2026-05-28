3178045924a6fa06ac5f3badfd5ac4d32f6068a4
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-05-28 14:59:54 +0530

    Images component

--- nextjs/app/(main)/contact/page.tsx
+++ nextjs/app/(main)/contact/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const ContactPage = () => {
-  return <div>This is Contact Page</div>;
+  return (
+    <div>
+      <Image
+        src="/under-construction.svg"
+        width={500}
+        height={500}
+        alt="Picture of Page Under Construction"
+      />
+      <h1>Contact page will be available soon.</h1>
+      <Link href={"/"} replace={false}>
+        Go back to Home Page
+      </Link>
+
+      <Image
+        src={"https://avatars.githubusercontent.com/u/156114974?v=4"}
+        width={500}
+        height={500}
+        alt="Picturefrom third part resource"
+      />
+    </div>
+  );
 };
 
 export default ContactPage;
