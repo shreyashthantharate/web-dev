67e987d666f1a4d64a5262326a5275c1c4d2a470
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-05-28 14:26:57 +0530

    Link Component

--- nextjs/app/(main)/about/page.tsx
+++ nextjs/app/(main)/about/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
 const AboutPage = () => {
-  return <div>Hello from AboutPage</div>;
+  return (
+    <div>
+      <h1>Hello from AboutPage</h1>
+      <Link href={"/contact"}>Go to Contact</Link> <br />
+      <Link href={"/products/prod-1"} replace={true}>
+        Go to Product page
+      </Link>
+      <br />
+      <Link
+        href={{
+          pathname: "/contact",
+          query: { name: "test" },
+        }}
+      >
+        Contact with query
+      </Link>
+    </div>
+  );
 };
 
 export default AboutPage;
