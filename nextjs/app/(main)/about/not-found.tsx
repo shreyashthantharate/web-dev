b7b3265278203eb20dfb25584093cf749760273b
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-05-28 15:57:37 +0530

    Error page, Not Found page, Loading page

---
+++ nextjs/app/(main)/about/not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Dashboard page not found</h2>
+      <p>Check the URL or go back.</p>
+    </div>
+  );
+}
---
+++ nextjs/app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomNotFound = () => {
+  return <div>CustomNotFound</div>;
+};
+
+export default CustomNotFound;
