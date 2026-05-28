1396e498b983ceda4fecb64d28df2a24a54e2562
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-05-28 15:57:16 +0530

    Custom fonts

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,16 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,10 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={` ${poppins.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <h1>Header</h1>
         {children}
--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -4,6 +4,14 @@ export default function Home() {
   return (
     <div>
       <h1>Hello world!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, minus eum
+        illo odio illum cumque fugiat. Deleniti, dicta? Debitis perspiciatis
+        tempora autem molestias. Aperiam dignissimos dicta nam neque sunt
+        corrupti quidem ea! Explicabo porro magni maiores in minus, ad
+        voluptatem voluptate asperiores placeat, cum facilis eveniet doloremque?
+        Hic, nemo. Laboriosam?
+      </p>
     </div>
   );
 }
