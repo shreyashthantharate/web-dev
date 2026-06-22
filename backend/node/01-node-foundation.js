f7bd17af2115aafd8b95c756eea03f14fc223423
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-06-23 00:02:19 +0530

    typescript and js backend refresher.

---
+++ backend/node/01-node-foundation.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+
+// console.log("NodeJS ", process.versions.node);
+// console.log("V8 ", process.versions.v8);
+// console.log("libuv ", process.versions.uv);
+// console.log("===========================");
+// console.log("platform", process.platform);
+// console.log("CPU", os.cpus().length);
+
+console.log(global);
+
+console.log(globalThis);
