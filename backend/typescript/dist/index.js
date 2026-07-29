2a81519457e67e61dffebd4529acb53953939415
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-07-30 00:41:03 +0530

    Remove old ts files

--- backend/typescript/dist/index.js
+++
@@ -1,7 +0,0 @@
-function greet(person) {
-    return `Hello ${person}, Welcome to typescript world.`;
-}
-const username = "TypeScript Buddy";
-console.log(greet(username));
-export {};
-//# sourceMappingURL=index.js.map
--- backend/typescript/src/index.ts
+++
@@ -1,7 +0,0 @@
-function greet(person: string): string {
-  return `Hello ${person}, Welcome to typescript world.`;
-}
-
-const username: string = "TypeScript Buddy";
-
-console.log(greet(username));
