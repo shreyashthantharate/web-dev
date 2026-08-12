c16128237459d2e8413bbc26a108f08170d93c3f
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-08-12 19:44:33 +0530

    phone_number data type

--- backend/sql/01.sql
+++ backend/sql/01.sql
@@ -6,7 +6,7 @@ CREATE TABLE students(
   last_name VARCHAR(50), --(50 is bytes) 50 * 8 bits
 
   email VARCHAR(322) UNIQUE NOT NULL,
-  phone_number CHAR(10) UNIQUE,  -- 4 bytes vs 10 bytes
+  phone_number CHAR(10) UNIQUE,  -- INT  --> 4 bytes vs BIGINT  --> 8 bytes vs VARCHA  --> 10 bytes
   country_code VARCHAR(4),
 
   age INT CHECK(age > 12), -- check age should be greater then 12
