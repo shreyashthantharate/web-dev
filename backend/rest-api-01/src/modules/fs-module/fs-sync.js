4788b745e915d206fbfa90213d9db14921a3aabb
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-08-20 00:11:06 +0530

    Fs module: Create, Write, Read, Append, Delete

---
+++ backend/rest-api-01/src/modules/fs-module/fs-sync.js
@@ -0,0 +1,31 @@
+import fs from "node:fs";
+
+// To create, read, update, delete or to work with directories we use fs modules
+
+// We use it with sync, async and promises methods
+// sync => one task at a time
+// async => multiple work at sametime (multi tasking)
+//  promises =>  to deal with callback
+
+// 1. WRITE
+
+// Create file if not exist and wirte in file
+// fs.writeFileSync("test.txt", "Hello from sync fs!");
+
+// 2. READ
+// Read the content in file, encoding is necessary for correct output
+// const data = fs.readFileSync("test.txt", "utf-8");
+// console.log(data);
+
+// 3. Append
+//  To add data in new line not wver ride existing data
+// fs.appendFileSync("test.txt", "\nHow are you?");
+//  "\n" to; go to new line
+
+// Create Folder
+// fs.mkdirSync("myFolder/innerFolder", { recursive: true });
+// { recursive: true } this make sure the "myFolder" must exist before creating "innerFolder"
+
+// Delete
+// To delete the file
+// fs.unlinkSync("test.txt");
